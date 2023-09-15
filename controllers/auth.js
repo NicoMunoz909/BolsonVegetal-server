37b226b7da495c2cf77f635e0bdcf4c6c1b5a993
NicoMunoz909 <nimunoz28@gmail.com>
2023-09-15 14:51:34 -0500

    Remove debug logs

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,19 +6,15 @@ const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) return res.status(400).send("Username or password missing");
   try {
-    console.log("Try statement beggining");
     const user = await User.findOne({ where: { username } });
-    console.log("Try statement after sequelize call");
     if (user && user.validatePassword(password)) {
-      console.log("Try statement inside password validation");
       const token = jwt.sign({ username, password }, process.env.TOKEN_SECRET);
-      console.log("Try statement after jwt sign");
       res.send({ msg: "Logged In Succesfully", token });
     } else {
       res.status(400).send("Contraseña incorrecta");
     }
   } catch (error) {
-    res.status(400).send("ACAAAAAAAAAA");
+    res.status(400).send(error);
   }
 };
 
