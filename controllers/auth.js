1ff85568b4a7440caf71d64db1efdbdaccdb5fe1
NicoMunoz909 <nimunoz28@gmail.com>
2023-09-15 14:36:00 -0500

    Add logs go debug

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,17 +4,17 @@ const { User } = require("../db/models");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) return res.status(403).send("Username or password missing");
+  if (!username || !password) return res.status(400).send("Username or password missing");
   try {
     const user = await User.findOne({ where: { username } });
     if (user && user.validatePassword(password)) {
       const token = jwt.sign({ username, password }, process.env.TOKEN_SECRET);
       res.send({ msg: "Logged In Succesfully", token });
     } else {
-      res.status(401).send("Contraseña incorrecta");
+      res.status(400).send("Contraseña incorrecta");
     }
   } catch (error) {
-    res.status(402).send(error);
+    res.status(400).send("ACAAAAAAAAAA");
   }
 };
 
