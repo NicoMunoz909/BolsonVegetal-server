ec35235bba6ffcc3a870d77934006f9a6489c6c3
NicoMunoz909 <nicolas.munoz@outlook.es>
2025-02-15 20:21:08 -0500

    Add port to db config

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: "snj627IRM162",
+    password: "snj627IRM162!",
     database: "BolsonVegetal",
     host: "127.0.0.1",
     dialect: "mysql",
@@ -19,6 +19,7 @@ module.exports = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOSTNAME,
     dialect: "mysql",
   },
